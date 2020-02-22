29cdf0d628cde72f8a5dfa850abba4df0b279da6
Ananya015 <jainanaya78@gla.ac.in>
2020-02-22 18:43:05 +0530

    make addFirst method

--- src/definition/LinkedList.java
+++ src/definition/LinkedList.java
@@ -1,7 +1,30 @@
 package definition;
 
-public class LinkedList {
-  private static class Node<E>{
+import Adt.MyLinkListAdt;
+
+public class LinkedList<E> implements MyLinkListAdt<E> {
+    private Node<E> head=null;
+    private int size=0;
+    private void addFirst(E item){
+        head=new Node<> (  item,head);
+        size++;
+    }
+    @Override
+    public void add(Object item) {
+
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E getData(int index) {
+        return null;
+    }
+
+    private static class Node<E>{
       private E data;
       private Node<E> next;
       public Node(E data, Node<E> next) {
