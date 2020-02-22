990c10328edab92afdbd82016be7574d441acf19
Ananya015 <jainanaya78@gla.ac.in>
2020-02-22 19:28:13 +0530

    make getter

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -5,10 +5,27 @@ public class Person {
     private String lastName;
     private String Email;
    private LinkedList<String> ContactNumbers;
+
     public Person(String firstName, String lastName, String email, LinkedList<String> contactNumbers) {
         this.firstName = firstName;
         this.lastName = lastName;
         Email = email;
         ContactNumbers = contactNumbers;
+
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public LinkedList<String> getContactNumbers() {
+        return ContactNumbers;
     }
 }
