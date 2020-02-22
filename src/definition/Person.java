f9bc441b1d4beee985a50e09c8472c639d327119
Ananya015 <jainanaya78@gla.ac.in>
2020-02-22 19:27:22 +0530

    make constructor

--- src/definition/Person.java
+++ src/definition/Person.java
@@ -1,5 +1,14 @@
 package definition;
 
 public class Person {
-
+    private String firstName;
+    private String lastName;
+    private String Email;
+   private LinkedList<String> ContactNumbers;
+    public Person(String firstName, String lastName, String email, LinkedList<String> contactNumbers) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        Email = email;
+        ContactNumbers = contactNumbers;
+    }
 }
