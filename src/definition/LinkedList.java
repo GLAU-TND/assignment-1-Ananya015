881375c7cd5a11c4af4710e866784e0130e42afb
Ananya015 <jainanaya78@gla.ac.in>
2020-02-22 18:29:40 +0530

    make constructor in node class

--- src/definition/LinkedList.java
+++ src/definition/LinkedList.java
@@ -4,6 +4,12 @@ public class LinkedList {
   private static class Node<E>{
       private E data;
       private Node<E> next;
+      public Node(E data, Node<E> next) {
+          this.data = data;
+          this.next = next;
+      }
+
+
 
   }
 }
