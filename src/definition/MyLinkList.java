d5ec1123deadd2b20e936119244a7fdcbe813fec
Ananya015 <jainanaya78@gla.ac.in>
2020-02-22 15:39:21 +0530

    override methods of MyLinkListAdt

---
+++ src/definition/MyLinkList.java
@@ -0,0 +1,34 @@
+package definition;
+
+import Adt.MyContactListAdt;
+
+import java.util.Scanner;
+
+public class MyLinkList implements MyContactListAdt {
+    Scanner sc = new Scanner ( System.in );
+
+    @Override
+    public void addContact() {
+
+    }
+
+    @Override
+    public void viewContact() {
+
+    }
+
+    @Override
+    public void deleteContact() {
+
+    }
+
+    @Override
+    public void searchContact() {
+
+    }
+
+    private static class Node<E>
+    {
+
+    }
+}
