52bbecfb3624ade70b6b250084313f6af368f32b
Ananya015 <jainanaya78@gla.ac.in>
2020-02-22 21:42:55 +0530

    return getData

--- src/definition/LinkedList.java
+++ src/definition/LinkedList.java
@@ -4,7 +4,7 @@ import Adt.MyLinkListAdt;
 
 public class LinkedList<E> implements MyLinkListAdt<E> {
     private Node<E> head=null;
-    private int size=0;
+    public int size=0;
 
     private void addFirst(E item)
     {
@@ -87,7 +87,7 @@ public class LinkedList<E> implements MyLinkListAdt<E> {
 
     @Override
     public E getData(int index) {
-        return null;
+        return getNode ( index ).getData ();
     }
 
     private static class Node<E>{
