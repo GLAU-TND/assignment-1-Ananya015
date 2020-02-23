04e5e129ce97bcd6ec4614058eee682d1e30877b
Ananya015 <jainanaya78@gla.ac.in>
2020-02-23 17:33:55 +0530

    add getFirstName method in MyLinkList class

--- src/definition/MyLinkList.java
+++ src/definition/MyLinkList.java
@@ -6,6 +6,13 @@ import java.util.Scanner;
 
 public class MyLinkList implements MyContactListAdt {
     Scanner sc = new Scanner ( System.in );
+    private String getFirstName()
+    {
+        System.out.println ("Please enter the Name of Person");
+        System.out.println ("First Name: ");
+        String fName=sc.next ();
+        return fName;
+    }
 
     @Override
     public void addContact() {
