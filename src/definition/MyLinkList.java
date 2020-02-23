21d9886dbc1cc7fb14d53912f3758e7381d2ff96
Ananya015 <jainanaya78@gla.ac.in>
2020-02-23 18:45:21 +0530

    add getcontact method

--- src/definition/MyLinkList.java
+++ src/definition/MyLinkList.java
@@ -3,6 +3,7 @@ package definition;
 import Adt.MyContactListAdt;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class MyLinkList implements MyContactListAdt {
     Scanner sc = new Scanner ( System.in );
@@ -18,6 +19,51 @@ public class MyLinkList implements MyContactListAdt {
         System.out.println ("LastName: ");
         String lName=sc.next ();
         return lName;
+    }
+    private LinkedList<String> getContactNumbers(){
+        LinkedList<String> contactNumbers=new LinkedList<> ();
+        System.out.println ("Contact Number");
+        String contactNum=sc.next ();
+        while(true)
+        {
+          if(Pattern.matches ( "[0-9]+",contactNum )){
+              contactNumbers.add ( contactNum );
+              break;
+        }
+        else{
+            System.out.println ("Invalid PhoneNumber");
+            break;
+        }
+        }
+        while(true){
+            System.out.println ("Do You want to add new Contact Number? (y/n) :");
+            String s=sc.next();
+            char at=s.charAt(0);
+            if(s.length ()>1){
+                System.out.println ("Please enter a valid input i.e. , y(lowercase) for Yes or n(lowercase for No)");
+                continue;
+            }
+            if(at=='y'){
+                System.out.println ("contactNumber: ");
+                contactNum =sc.next ();
+                if(Pattern.matches ( "[0-9]+",contactNum))
+                {
+                    contactNumbers.add(contactNum);
+                }
+                else
+                    {
+                    System.out.println ("Invalid PhoneNumber");
+                    }
+            }
+            else if(at=='n'){
+                break;
+            }
+            else{
+                System.out.println ("Please Enter a Vaild Input i.e., y(lowercase) for Yes or n(lowercase for NO");
+            }
+        }
+        return contactNumbers;
+
     }
 
     @Override
